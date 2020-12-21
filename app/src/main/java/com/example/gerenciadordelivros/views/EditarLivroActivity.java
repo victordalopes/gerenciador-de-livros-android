19cbbd135cc446d55dec84cccd25f6e26e67b6fc
Victor Lopes <victao_lopes@hotmail.com>
2020-12-21 02:12:00 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/gerenciadordelivros/views/EditarLivroActivity.java
@@ -0,0 +1,82 @@
+package com.example.gerenciadordelivros.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.gerenciadordelivros.R;
+import com.example.gerenciadordelivros.data.LivroDAO;
+import com.example.gerenciadordelivros.dominio.Livro;
+
+public class EditarLivroActivity extends AppCompatActivity {
+
+    private EditText edt_titulo;
+    private EditText edt_autor;
+    private EditText edt_editora;
+    private CheckBox chk_emprestado;
+
+    private LivroDAO livroDAO;
+
+    private Livro livro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_livro);
+
+        edt_titulo = findViewById(R.id.edt_titulo);
+        edt_autor = findViewById(R.id.edt_autor);
+        edt_editora = findViewById(R.id.edt_editora);
+        chk_emprestado = findViewById(R.id.check_emprestado);
+
+        livroDAO = LivroDAO.getInstance(this);
+
+        livro = (Livro) getIntent().getSerializableExtra("livro");
+
+        if(livro != null){
+            edt_titulo.setText(livro.getTitulo());
+            edt_autor.setText(livro.getAutor());
+            edt_editora.setText(livro.getEditora());
+            chk_emprestado.setChecked((livro.getEmprestado()==1) ? true : false);
+        }
+
+    }
+
+    public void cancelar(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    public void processar(View view) {
+
+        String titulo = edt_titulo.getText().toString();
+        String autor = edt_autor.getText().toString();
+        String editora = edt_editora.getText().toString();
+        int emprestado = (chk_emprestado.isChecked()) ? 1 : 0;
+
+        String msg;
+
+        if (livro == null) {
+            Livro livro = new Livro(titulo, autor, editora, emprestado);
+            livroDAO.save(livro);
+            msg = "Livro adicionado com sucesso! ID= "+livro.getId();
+        }else{
+            livro.setTitulo(titulo);
+            livro.setAutor(autor);
+            livro.setEditora(editora);
+            livro.setEmprestado(emprestado);
+
+            livroDAO.update(livro);
+            msg = "Livro atualizado com sucesso! ID= "+livro.getId();
+        }
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        setResult(RESULT_OK);
+        finish();
+
+    }
+}
---
+++ app/src/main/java/com/example/gerenciadordelivros/views/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.gerenciadordelivros.views;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.gerenciadordelivros.R;
+import com.example.gerenciadordelivros.adapter.LivroAdapter;
+import com.example.gerenciadordelivros.data.LivroDAO;
+import com.example.gerenciadordelivros.dialogs.DeleteDialog;
+import com.example.gerenciadordelivros.dominio.Livro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements LivroAdapter.OnLivroListener, DeleteDialog.OnDeleteListener {
+
+    private LivroDAO livroDAO;
+    LivroAdapter livroAdapter;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerview = findViewById(R.id.recyclerview);
+
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+//        Dados antigos usados para preencher o banco antes da criação da classe privada livroDAO
+
+//        listaLivros.add(new Livro(1L,"Android para Leigos","Michael Burton","Alta books",0));
+//        listaLivros.add(new Livro(2L,"Android para Programadores","Paul J, Deitel","Bookman",1));
+//        listaLivros.add(new Livro(3L,"Desenvolvimento para Android","Griffiths, David","Alta books",0));
+//        listaLivros.add(new Livro(4L,"Android Base de Dados","Queirós, Ricardo","FCA Editora",1));
+//        listaLivros.add(new Livro(5L,"Android em Ação","King, Chris","Elsevier - Campus",0));
+//        listaLivros.add(new Livro(6L,"Jogos em Android","Queirós, Ricardo","FCA - Editora",1));
+//        listaLivros.add(new Livro(7L,"Android Essencial com Kotlin","Ricardo R.","NOVATEC",0));
+
+        livroDAO = LivroDAO.getInstance(this);
+
+        List<Livro> listaLivros = livroDAO.list();
+
+        livroAdapter = new LivroAdapter(listaLivros,this, this);
+
+        recyclerview.setAdapter(livroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.actions,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_adicionar:
+                Intent intent = new Intent(getApplicationContext(),EditarLivroActivity.class);
+                startActivityForResult(intent, 100);
+                return true;
+            case R.id.action_sair:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+                atualizaListaLivros();
+        }
+        if(requestCode == 101 && resultCode == RESULT_OK){
+            atualizaListaLivros();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void atualizaListaLivros(){
+        List<Livro> livros = livroDAO.list();
+        livroAdapter.setItems(livros);
+        livroAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onLivroClick(int posicao) {
+
+        Intent intent = new Intent(getApplicationContext(),EditarLivroActivity.class);
+        intent.putExtra("livro",livroAdapter.getItem(posicao));
+        startActivityForResult(intent, 101);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onLivroLongClick(int posicao) {
+
+        Livro livro = livroAdapter.getItem(posicao);
+
+        DeleteDialog dialog = new DeleteDialog();
+        dialog.setLivro(livro);
+        dialog.show(getSupportFragmentManager(), "deleteDialog");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onDelete(Livro livro) {
+        livroDAO.delete(livro);;
+        atualizaListaLivros();
+
+        Toast.makeText(this,"Livro excluído com sucesso!", Toast.LENGTH_LONG).show();
+
+    }
+}
